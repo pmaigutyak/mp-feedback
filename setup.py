f194e8e227d88128e45e164e95a94bc638df3a08
pmaigutyak <pmaigutyak@gmail.com>
2020-04-20 20:22:06 +0300

    Fix setup.

--- setup.py
+++ setup.py
@@ -5,19 +5,15 @@ from setuptools import setup, find_packages
 version = '0.1'
 url = 'https://github.com/pmaigutyak/mp-feedback'
 
-with open('requirements.txt') as f:
-    requires = f.read().splitlines()
-
 setup(
     name='django-mp-feedback',
     version=version,
-    description='Django admin app',
+    description='Django feedback app',
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
     url=url,
     download_url='{}/archive/{}.tar.gz'.format(url, version),
     packages=find_packages(),
     include_package_data=True,
-    license='MIT',
-    install_requires=requires
+    license='MIT'
 )
